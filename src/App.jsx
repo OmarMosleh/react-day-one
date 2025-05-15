5e719269b5385880fe3ed56d25ba6e84da5ee29f
OmarMosleh <omarmosleh90@gmail.com>
2025-05-15 12:19:46 +0300

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "./components/NavBar";
 import User from "./components/User";
+import { useEffect } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [seconds, setSecondes] =useState(0)
+  const [userName, setUserName] =useState("omar")
+  useEffect(() => {
+    console.log("hello from effect");
+  },[]);
+
+
+
+  useEffect(()=>{
+    const interval =setInterval(()=>{
+        setSecondes((prevSeconds)=> prevSeconds + 1)
+    },1000)
+
+    return () => clearInterval(interval); // to stop the interval when the component unmount
+  },[])
+  // without [] ==> with each rerender --not recommended
+  // with [] ==> with first rerender Only --ex. get allusers
+  // with [dependency] ==> whenever this dependency change 
+
+
+  const increaseCount = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
   return (
     <>
-      <User />
+      <h1>{userName}</h1>
+      <button onClick={increaseCount}>increase count</button>
+      <h1>Hello effect</h1>
+      <h1>{count}</h1>
+      <h1>Seconds : {seconds}</h1>
     </>
   );
 }
